9a43336a4fb96d32a109b25d9eae9ab149b8c053
Theodoros Gkisis <82917321+TheodoreGisis@users.noreply.github.com>
2024-05-14 14:13:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
+# terraform {
+#   backend "s3" {
+#     bucket = "tf-remote-bucket-milestone-1-theodoros-gkisis1"
+#     key = "tf-remote-bucket-milestone-1-theodoros-gkisis/MILE1/terraform/tfstate"
+#     region = "eu-central-1"
+#     dynamodb_table = "tf-lock-table"
+#   }
+# }
+
 provider "aws" {
   region = "eu-central-1"
 }
-
 
 locals {
   common_tags = {
